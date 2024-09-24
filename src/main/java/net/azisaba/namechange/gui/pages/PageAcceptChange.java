ac442f1ec1f87184c671b7f3cd5ade096946c170
Arisa9006 <57523862+Arisa9006@users.noreply.github.com>
2024-09-24 14:36:56 +0900

    AcceptGuiのアイテムを設定

--- src/main/java/net/azisaba/namechange/gui/pages/PageAcceptChange.java
+++ src/main/java/net/azisaba/namechange/gui/pages/PageAcceptChange.java
@@ -1,12 +1,18 @@
 package net.azisaba.namechange.gui.pages;
 
+import com.shampaggon.crackshot.CSUtility;
 import net.azisaba.namechange.data.WaitingAcceptData;
 import net.azisaba.namechange.gui.GuiPage;
 import net.azisaba.namechange.gui.InventoryGui;
+import net.azisaba.namechange.gui.items.ItemAccept;
+import net.azisaba.namechange.gui.items.ItemDeny;
+import net.azisaba.namechange.gui.items.ItemSign;
+import org.bukkit.inventory.ItemStack;
 
 public class PageAcceptChange extends GuiPage {
 
     private final WaitingAcceptData data;
+
 
     public PageAcceptChange(InventoryGui gui, WaitingAcceptData data) {
         super(gui, name, size);
@@ -14,7 +20,14 @@ public class PageAcceptChange extends GuiPage {
 
     @Override
     public void setUp() {
-
+        CSUtility util = new CSUtility();
+        ItemStack beforeItem = util.generateWeapon(data.getPreviousID());
+        ItemStack afterItem = util.generateWeapon(data.getNewID());
+        this.setItem(10, beforeItem);
+        this.setItem(11, new ItemSign(gui));//GUI名が必要？
+        this.setItem(12, afterItem);
+        this.setItem(14, new ItemAccept(gui,data));
+        this.setItem(16, new ItemDeny(gui,data));
     }
 
     @Override
